700bb7c744849680136d5e2faafe7897d115f5d2
DAVID FERRERO VEGA <david.ferrero.contractor@dev.bbva.com>
2019-02-06 15:57:28 +0100

    Jenkins Server

--- Jenkinsfile
+++ Jenkinsfile
@@ -10,7 +10,7 @@ pipeline {
     }
     stage('Test') {
       steps {
-        sh 'npm --version'
+        sh 'npm run test-google'
       }
     }
   }
--- spec/checkRole.js
+++ spec/checkRole.js
@@ -2,7 +2,10 @@ const { Builder } = require('selenium-webdriver');
 const { expect } = require('chai');
 
 describe('Check Example', () => {
-  const driver = new Builder().forBrowser('chrome').build();
+  const driver = new Builder()
+    .usingServer('http://localhost:49002/wd/hub')
+    .forBrowser('chrome')
+    .build();
   it('Check Title', async () => {
     await driver.get('https://www.google.com/');
     const title = await driver.getTitle();
